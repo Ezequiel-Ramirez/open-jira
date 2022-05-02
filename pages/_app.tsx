4a04d834bb9f115235d49272901c749a5bbde99b
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2022-05-02 10:43:35 -0300

    se genera context para manejo de menu open - close

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import { UIProvider } from "../context/ui";
 import "../styles/globals.css";
 import { darkTheme, lightTheme } from "../themes";
 
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UIProvider sidemenuOpen={false}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
   );
 }
 
